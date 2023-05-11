591e2aa3b2fbd82c36af1e751dc608fbec5adcfb
Dwi Putra Bayu <dwiputrabayu19@gmail.com>
2023-05-12 01:21:13 +0700

    template finish

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Nav Start -->
     <?php require 'partials/nav.php' ?>
     <!-- Nav End -->
-    <div class="content">
+    <div class="main">
         <!-- Section 1 Start -->
         <section class="section-1">
             <h2 class="mb-4">SEBUAH TITIK; PORTFOLIO</h2>
@@ -20,99 +20,110 @@
         <!-- Section 1 End -->
         <!-- Section About Start -->
         <section class="section-2">
-            <div class="row">
-                <div class="col-12 d-flex flex-column-reverse">
+            <div class="row d-flex justify-content-center w-100">
+                <div class="col-12 content-about-1">
                     <div class="title">
                         <p>SIAPA YANG MENGENAL TUHANNYA, SESUNGGUHNYA IA MENGENAL DIRINYA</p>
                     </div>
                     <img class="img-about-1" src="assets/img/ridwan.jpg" alt="Photo About" width="500" height="300">
-                    <p class="fs-1">02</p>
+                    <p class="fs-1 text-end">02</p>
                 </div>
-                <div class="col-12 d-flex flex-column-reverse">
+                <div class="col-12 content-about-2 d-flex flex-column flex-md-row">
                     <div class="description">
                         <p>Saya adalah nomad digital yang saat ini berbasis di Bandung. Saya telah berkecimpung di dunia
                             desain grafis selama sepuluh tahun. Baru tiga tahun terakhir ini saya memutuskan untuk fokus
                             sepenuhnya pada ilustrasi.</p>
                     </div>
-                    <img class="img-about-2" src="assets/img/ridwan.jpg" alt="Photo About" height="400" width="300">
+                    <img class="img-about-2" src="assets/img/ridwan.jpg" alt="Photo About" width="200">
                 </div>
             </div>
         </section>
         <!-- Section About End -->
         <!-- Section Resume Start -->
         <section class="section-3">
-            <div class="row">
-                <div class="col-12 d-flex flex-column-reverse">
-                    <div class="title">
-                        <p>PENGALAMAN KERJA</p>
-                    </div>
-                    <p class="fs-1">03</p>
-                </div>
-                <div class="col-12 d-flex flex-column">
-                    <div class="col-12 my-3">
-                        <div class="professional">
-                            <h6 class="fw-800 fs-3">Kerja Agensi</h6>
-                            <p class="text mb-1">Studio Grafis Ganesha</p>
-                            <p class="text mb-1" class="mb-0">- Pimpinan Kreatif (2020-2022)</p>
-                            <p class="text mb-1">- Pengarah Seni (2018-2020)</p>
-                            <p class="text mb-1">Studio Grafis Ganesha</p>
-                            <p class="text mb-1" class="mb-0">- Pimpinan Kreatif (2020-2022)</p>
-                            <p class="text mb-1">- Pengarah Seni (2018-2020)</p>
+            <div class="content-resume">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-flex justify-content-between w-100">
+                            <div class="title">
+                                <p>PENGALAMAN KERJA</p>
+                            </div>
+                            <p class="fs-1">03</p>
                         </div>
                     </div>
-                    <div class="col-12 my-3">
-                        <div class="freelance">
-                            <h6 class="fw-800 fs-3">Kerja Lepas</h6>
-                            <p class="mb-0 text mb-1">ILUSTRATOR PURNAWAKTU</p>
-                            <p class="text mb-1">(2022 - SEKARANG)</p>
-                            <p class="mb-0 text mb-1">DESAINER GRAFIS & ILUSTRATOR PARUH WAKTU</p>
-                            <p class="text mb-1">(2021-2022)</p>
+                    <div class="col-12 mb-5 d-flex flex-column flex-md-row">
+                        <div class="col-12 col-sm-4 my-3">
+                            <div class="professional">
+                                <h6 class="fw-800 fs-3">Kerja Agensi</h6>
+                                <p class="text mb-1">Studio Grafis Ganesha</p>
+                                <p class="text mb-1" class="mb-0">- Pimpinan Kreatif (2020-2022)</p>
+                                <p class="text mb-1">- Pengarah Seni (2018-2020)</p>
+                                <p class="text mb-1">Studio Grafis Ganesha</p>
+                                <p class="text mb-1" class="mb-0">- Pimpinan Kreatif (2020-2022)</p>
+                                <p class="text mb-1">- Pengarah Seni (2018-2020)</p>
+                            </div>
                         </div>
+                        <div class="col-12 col-sm-4 my-3">
+                            <div class="freelance">
+                                <h6 class="fw-800 fs-3">Kerja Lepas</h6>
+                                <p class="mb-0 text mb-1">ILUSTRATOR PURNAWAKTU</p>
+                                <p class="text mb-1">(2022 - SEKARANG)</p>
+                                <p class="mb-0 text mb-1">DESAINER GRAFIS & ILUSTRATOR PARUH WAKTU</p>
+                                <p class="text mb-1">(2021-2022)</p>
+                            </div>
+                        </div>
+                        <img class="col-12 col-sm-4" src="assets/img/ridwan.jpg" alt="Photo About">
                     </div>
-                    <img class="img-about-2 col-12 col-sm-12" src="assets/img/ridwan.jpg" alt="Photo About" height="300"
-                        width="300">
                 </div>
             </div>
         </section>
         <!-- Section Resume End -->
         <!-- Section Contact Start -->
         <section class="section-4">
-            <div class="row position-relative">
-                <div class="col-12 d-flex flex-column-reverse">
-                    <div class="title">
-                        <p>MARI KOLABORASI</p>
+            <div class="content-contact">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-between w-100">
+                        <div class="title">
+                            <p>MARI KOLABORASI</p>
+                        </div>
+                        <p class="fs-1">04</p>
                     </div>
-                    <p class="fs-1">04</p>
-                </div>
-                <div class="col-12 d-flex flex-column">
-                    <div class="col-8">
-                        <div class="contact">
-                            <p>Jl. Hidup Baru VII</p>
-                            <p>082115811184</p>
-                            <p>mekkaridwan@gmail.com</p>
+                    <div class="col-12">
+                        <div class="col-8">
+                            <div class="contact">
+                                <p>Jl. Hidup Baru VII</p>
+                                <p>082115811184</p>
+                                <p>mekkaridwan@gmail.com</p>
+                            </div>
+                        </div>
+                        <div class="col-4 img-contact">
+                            <img class="mt-3" src="assets/img/ridwan.jpg" alt="Photo Contact">
                         </div>
                     </div>
-                    <img class="col-4" src="assets/img/ridwan.jpg" alt="Photo Contact">
                 </div>
             </div>
         </section>
         <!-- Section Contact End -->
         <!-- Section 5 Start -->
         <section class="section-5">
-            <div class="col-12 text-center">
-                <p class="fs-1">05</p>
-            </div>
-            <div class="text-center">
-                <div class="col-12">
-                    <p class="text">SAYA</p>
-                </div>
-                <div class="col-12 d-flex justify-content-center">
-                    <p class="text-2">TERCIPTA</p>
-                    <img src="assets/img/ridwan.jpg" alt="Image" width="200">
-                </div>
-                <div class="col-12 d-flex justify-content-center">
-                    <img src="assets/img/ridwan.jpg" alt="Image" width="200">
-                    <p class="text">MENCIPTA</p>
+            <div class="content-5">
+                <div class="row">
+                    <div class="col-12 text-end">
+                        <p class="fs-1">05</p>
+                    </div>
+                    <div class="text-center">
+                        <div class="col-12">
+                            <p class="text">SAYA</p>
+                        </div>
+                        <div class="col-12 d-flex justify-content-center gap-3 align-items-center">
+                            <p class="text-2">TERCIPTA</p>
+                            <img src="assets/img/ridwan.jpg" alt="Image" width="500" height="200">
+                        </div>
+                        <div class="col-12 d-flex justify-content-center gap-3 align-items-center">
+                            <img src="assets/img/ridwan.jpg" alt="Image" width="500" height="200">
+                            <p class="text">MENCIPTA</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
--- partials/nav.php
+++ partials/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-white fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">M. Eka Ridwan K.</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -11,21 +11,13 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">Resume</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown link
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
         </div>
